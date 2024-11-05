ddaf2d0bfb59290534517d7d15a980faa9bdc2e1
tauheed0110 <tauheed0110@gmail.com>
2024-11-05 15:36:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function validEmail(str) {
-  //your JS code here.
-}
-
-// Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+    //your JS code here.
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(str);
+  }
+  
+  // Do not change the code below.
+  const str = prompt("Enter an email address.");
+  alert(validEmail(str));
+  
